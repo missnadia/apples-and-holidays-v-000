4f4bbdbe61e68f46dbaa695902206523f0c9f299
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-14 02:12:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,9 +67,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect do |season, holidays|
     holidays.collect do |holiday, supplies|
-      holiday
-      if supplies.include?("BBQ")
-      end
+      holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
 end
