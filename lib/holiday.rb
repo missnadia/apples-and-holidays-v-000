fe9f8dc499b04f17246d841b55e3dcd6da66bc95
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-14 02:24:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,9 +55,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holidays|
-    puts "#{seasons.capitalize!}:"
-    holidays.collect do |holidays, supplies|
-    puts " #{holidays.to_s.split("_").capitalize!join(" ")}: #{supplies.join(", ")}"
+    puts "#{seasons.capitalize}:"
+    holidays.collect do |holiday, supplies|
+    puts " #{holiday.to_s.split("_").capitalize!join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
